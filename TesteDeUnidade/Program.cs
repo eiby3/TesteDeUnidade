a771e8843430e60a9b9a77ea2d6d961238914d97
Guilherme Abe <guilhermemarcondesabe@hotmail.com>
2022-02-01 14:15:09 -0300

    Add project files.

---
+++ TesteDeUnidade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(fizzBuzz.VerificarNumeros(i));
+            }
+        }
+    }
+}
